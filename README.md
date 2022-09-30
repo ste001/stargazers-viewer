44e948e6bd063625ffac9fb2120f3b6d7f60b097
Stefano Merazzi <s.merazzi@tinaba.it>
2022-09-30 10:05:19 +0200

    feat: add dotenv

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Stargazers Viewer
 
+## Configuration
+
+- Create a .env file
+- Copy the contents of the .env.example file inside the .env file
+- Add the provided environment variables
+
+## Run / Test
+
 Run the app
 
 `yarn start`
--- services/starring.ts
+++ services/starring.ts
@@ -1,7 +1,8 @@
 import { Octokit } from "@octokit/core";
+import { GITHUB_KEY } from "@env";
 
 const octokit = new Octokit({
-  auth: "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",
+  auth: GITHUB_KEY,
 });
 
 const getStargazers = async (owner: string, repo: string, page: number) => {
